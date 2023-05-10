f23881ec5f82638cd4fc62e657be8ef4a861f26f
John McAlpin <90294960+johnmcalpin@users.noreply.github.com>
2023-05-09 21:55:46 -0600

    Create semantic-schema-generator.py

---
+++ semantic-schema-generator.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import json
+
+st.title('Semantic Schema Markup Generator')
+
+# Input boxes
+about_data = st.text_area('About', help='Enter URLs line by line')
+mention_data = st.text_area('Mentions', help='Enter URLs line by line')
+website = st.text_input('Website', help='Enter your website URL')
+page_title = st.text_input('Page Title', help='Enter your page title')
+
+# Button to generate markup
+if st.button('Generate'):
+
+    # Split input into lines
+    about_urls = about_data.split('\n')
+    mention_urls = mention_data.split('\n')
+
+    # Create about and mention lists
+    about = [{'@type': 'Thing', 'name': url.split('/')[-1], 'sameAs': url} for url in about_urls]
+    mentions = [{'@type': 'Thing', 'name': url.split('/')[-1], 'sameAs': url} for url in mention_urls]
+
+    # Create schema
+    schema = {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "@id": website,
+        "headline": page_title,
+        "url": website,
+        "about": {
+            "@graph": about
+        },
+        "mentions": {
+            "@graph": mentions
+        }
+    }
+
+    # Print schema
+    schema_str = '<script type="application/ld+json">\n' + json.dumps(schema, indent=2) + '\n</script>'
+    st.code(schema_str, language='html')
